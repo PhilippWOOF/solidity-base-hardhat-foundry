32b0786ec9d609d30634efe193ef5de451731220
Yurii721 <86737052+Yurii721@users.noreply.github.com>
2024-12-12 08:25:03 +0200

    ci(package-prepare): add the initialization script

---
+++ scripts/commands/initialize.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+echo
+echo "Initializing a template..."
+
+INIT_FILE_PATH="./scripts/commands/.initialized"
+
+if [ ! -f "$INIT_FILE_PATH" ]; then
+    mkdir -p contracts test
+
+    rm -f LICENSE
+
+    touch "$INIT_FILE_PATH"
+
+    echo "Initialized."
+
+    # Prompt to commit changes after initialization.
+    overwrite=""
+    echo
+    echo "Commit changes after initialization? (y/n) -> "
+    read -r overwrite
+    echo
+
+    if [ "$overwrite" == "y" ]; then
+        git add .
+        git commit -m "chore: remove the template's license"
+    fi
+else
+    echo "The project has already been initialized."
+fi
+
+echo
