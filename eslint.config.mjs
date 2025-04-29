c357291daf52d83b8b0ff8814de5971feae5904f
Yurii721 <86737052+Yurii721@users.noreply.github.com>
2025-04-29 09:22:45 +0300

    fix(eslint): correct the config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,15 +8,8 @@ import globals from "globals";
 import path from "path";
 
 export default eslintTS.config(
-    { files: ["**/*.{ts,js,cjs}"] },
-    {
-        files: ["**/*.js"],
-        ...eslintTS.configs.disableTypeChecked,
-        languageOptions: { sourceType: "commonjs" }
-    },
-    {
-        ignores: ["**/*.mjs", "eslint.config.mjs", ".solcover.js", ".github/**/*", "ignition/deployments/**/*"]
-    },
+    { ignores: ["ignition/deployments/**/*"] },
+    { files: ["**/*.{ts,js}"] },
     eslintJS.configs.recommended,
     ...eslintTS.configs.strictTypeChecked,
     ...eslintTS.configs.stylisticTypeChecked,
@@ -24,7 +17,6 @@ export default eslintTS.config(
         rules: {
             quotes: ["error", "double"],
             "eol-last": ["error"],
-            "max-len": ["error", { code: 120, ignoreUrls: true }],
             "no-trailing-spaces": ["error"]
         }
     },
@@ -69,6 +61,17 @@ export default eslintTS.config(
                 projectService: true,
                 tsconfigRootDir: import.meta.dirname
             }
+        }
+    },
+    {
+        files: ["**/*.js", "**/*.mjs", "**/*.cjs"],
+        extends: [eslintTS.configs.disableTypeChecked]
+    },
+    {
+        files: ["**/*.js"],
+        languageOptions: { sourceType: "commonjs" },
+        rules: {
+            "@typescript-eslint/no-require-imports": "off"
         }
     },
     includeIgnoreFile(path.resolve(import.meta.dirname, ".gitignore"))
